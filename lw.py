a36ee453cbbf89c72b8d94f8ef20301487bbab49
nev <nev@neverix.io>
2024-10-14 19:22:10 -0400

    Add LessWrong icon

---
+++ lw.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#%%
+import requests 
+posts = [
+    "https://www.lesswrong.com/posts/8ev6coxChSWcxCDy8",
+    "https://www.lesswrong.com/posts/5FGXmJ3wqgGRcbyH7",
+    "https://www.lesswrong.com/posts/k8bBx4HcTF9iyikma"
+]
+# options:
+# 1 add lesswrong link
+# 2 generate "linkposts"
+# 3 scrape and pandoc whole posts
+# will just do 1 for now
